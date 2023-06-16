e5a693f9802ad569588e21d73ccd6ab2bffe004e
KaraFeiKa <prays99@mail.ru>
2023-06-16 11:07:39 +0300

    16.06.23

--- app/src/main/java/es/neci_desarrollo/applicationtest/MainActivity.java
+++ app/src/main/java/es/neci_desarrollo/applicationtest/MainActivity.java
@@ -11,24 +11,30 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.telephony.CellInfo;
+import android.telephony.CellInfoGsm;
+import android.telephony.CellInfoLte;
+import android.telephony.CellInfoWcdma;
 import android.telephony.CellSignalStrengthLte;
+import android.telephony.NeighboringCellInfo;
 import android.telephony.PhoneStateListener;
 import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.widget.TextView;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class MainActivity extends AppCompatActivity implements LocationListenerInterface {
     TelephonyManager telephonyManager;
     private LocationManager locationManager;
     private MyLocationListener myLocationListener;
-    TextView latitude_res;
-    TextView longitude_res;
-    TextView Mcc;
-    TextView Mnc;
-    TextView Ss;
-
-
+    TextView latitude_res,longitude_res,Mcc,Mnc,RSSI,RSRP,RSRQ,SNR;
 
 
     @Override
@@ -39,8 +45,8 @@ public class MainActivity extends AppCompatActivity implements LocationListenerI
         getLocation();
 
         if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-        && ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_PHONE_STATE},100);
+                && ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_PHONE_STATE}, 100);
         }
     }
 
@@ -49,7 +55,7 @@ public class MainActivity extends AppCompatActivity implements LocationListenerI
     private void getLocation() {
         try {
             locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,myLocationListener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, myLocationListener);
 
         } catch (Exception e) {
 
@@ -57,8 +63,7 @@ public class MainActivity extends AppCompatActivity implements LocationListenerI
     }
 
 
-    private void init()
-    {
+    private void init() {
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         myLocationListener = new MyLocationListener();
         myLocationListener.setLocationListenerInterface(this);
@@ -66,17 +71,49 @@ public class MainActivity extends AppCompatActivity implements LocationListenerI
         longitude_res = findViewById(R.id.Res_longitude);
         Mnc = findViewById(R.id.MNC);
         Mcc = findViewById(R.id.MCC);
-        Ss = findViewById(R.id.Res_RSSI);
+        RSSI = findViewById(R.id.Res_RSSI);
+        RSRP = findViewById(R.id.Res_RSRP);
+        RSRQ = findViewById(R.id.Res_RSRQ);
+        SNR = findViewById(R.id.Res_SNR);
+
         telephonyManager = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
 
     }
 
     @Override
     public void onLocationChanged(Location location) {
-                if (location != null) {
-                    latitude_res.setText(String.valueOf(location.getLatitude()));
-                    longitude_res.setText(String.valueOf(location.getLongitude()));
-                    Ss.setText(""+telephonyManager.getSignalStrength().getCellSignalStrengths());
+        if (location != null) {
+            latitude_res.setText(String.valueOf(location.getLatitude()));
+            longitude_res.setText(String.valueOf(location.getLongitude()));
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+            }
+            List<CellInfo> cellInfoList = telephonyManager.getAllCellInfo();
+            Mnc.setText("Ok");
+//            Log.d("Cell Info", cellInfoList.toString());
+                    for (CellInfo cellInfo : cellInfoList)
+                    {
+
+                        if (cellInfo instanceof CellInfoLte)
+                        {
+                            Log.d("LTE. Cell Info", "RSSI: "+((CellInfoLte)cellInfo).getCellSignalStrength().getRssi());
+                        }
+                        if (cellInfo instanceof CellInfoWcdma)
+                        {
+                            Log.d("Wcdma. Cell Info", cellInfoList.toString());
+                        }
+                        if (cellInfo instanceof CellInfoGsm)
+                        {
+                            Log.d("Gsm. Cell Info", cellInfoList.toString());
+                        }
+                    }
+            //List<NeighboringCellInfo>
         }
     }
 }
