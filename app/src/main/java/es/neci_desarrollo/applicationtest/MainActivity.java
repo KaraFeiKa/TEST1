bf6aa68dcd2b7d7dc0409099f4eced5c2b13540d
KaraFeiKa <prays99@mail.ru>
2023-06-20 12:17:46 +0300

    20.06.23 V1.1

--- app/src/main/java/es/neci_desarrollo/applicationtest/MainActivity.java
+++ app/src/main/java/es/neci_desarrollo/applicationtest/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity implements LocationListenerI
     }
 
 
+
     private void init() {
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         myLocationListener = new MyLocationListener();
